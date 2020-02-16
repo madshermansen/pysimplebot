ab542a58baf8ad96af78287dd02b7aad6a872bec
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2020-02-16 12:13:03 +0100

    Custom code
    - From cc1-1000 user can enter custom code following https://discordpy.readthedocs.io/en/latest/index.html documentation

--- pysimplebot.py
+++ pysimplebot.py
@@ -8,28 +8,27 @@ Notes: Discord module required
 
 import random
 import discord
-# from discord.ext import commands # user = commands.Bot(command_prefix=".") bot example
+from discord.ext import commands
 
 
 class Client:
     def __init__(self):
-        self.joinalert = None
-        self.game = None
-        self.respond = None
-        self.onready = None
-        self.token = None
+        pass
 
+    # Function to call when starting bot
     def start(self):
         token = self.token
         client = discord.Client()
 
-        if "onready" in dir(self):
-            @client.event
-            async def on_ready():
+        # Runs when bot is ready
+        @client.event
+        async def on_ready():
+            if "onready" in dir(self):
                 eval(self.onready)
-                if "game" in dir(self):
-                    await client.change_presence(status=discord.Status.idle, activity=discord.Game(self.game))
+            if "game" in dir(self):
+                await client.change_presence(status=discord.Status.idle, activity=discord.Game(self.game))
 
+        # Respond to messages from user
         if "respond" in dir(self):
             @client.event
             async def on_message(message):
@@ -42,9 +41,12 @@ class Client:
                         if message.content.startswith(i[0][j]):
                             if len(i) == 1:
                                 await channel.send(i[1])
+                                return
                             if len(i) > 1:
                                 await channel.send(random.choice(i[1]))
+                                return
 
+        # Join alerts for new members joining server
         if "joinalert" in dir(self):
             @client.event
             async def on_member_join(member):
@@ -53,4 +55,22 @@ class Client:
                     to_send = eval(self.joinalert)
                     await server.system_channel.send(to_send)
 
+        # User running custom code
+        cc = []
+        for i in range(1000):
+            if "cc" + str(i) in dir(self):
+                cc.append("cc" + str(i))
+        for customcode in cc:
+            code = "self." + customcode
+            print(code)
+            exec(eval(code))
+
+
         client.run(token)
+
+class Bot:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
