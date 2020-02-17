80e19d8c458cd80be634a895c08e1e79a099659f
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2020-02-17 20:50:08 +0100

    Token start bot command
    - Token
    - Run
    - Customcode
    - description

--- pysimplebot.py
+++ pysimplebot.py
@@ -20,7 +20,7 @@ class Client:
         token = self.token
         client = discord.Client()
 
-        # Runs when bot is ready
+        # Runs when bot is ready (self.onready)
         @client.event
         async def on_ready():
             if "onready" in dir(self):
@@ -28,7 +28,7 @@ class Client:
             if "game" in dir(self):
                 await client.change_presence(status=discord.Status.idle, activity=discord.Game(self.game))
 
-        # Respond to messages from user
+        # Respond to messages from user (self.respond)
         if "respond" in dir(self):
             @client.event
             async def on_message(message):
@@ -46,7 +46,7 @@ class Client:
                                 await channel.send(random.choice(i[1]))
                                 return
 
-        # Join alerts for new members joining server
+        # Join alerts for new members joining server (self.joinalert)
         if "joinalert" in dir(self):
             @client.event
             async def on_member_join(member):
@@ -55,14 +55,13 @@ class Client:
                     to_send = eval(self.joinalert)
                     await server.system_channel.send(to_send)
 
-        # User running custom code
+        # User running custom code (self.cc1-1000)
         cc = []
         for i in range(1000):
             if "cc" + str(i) in dir(self):
                 cc.append("cc" + str(i))
         for customcode in cc:
             code = "self." + customcode
-            print(code)
             exec(eval(code))
 
 
@@ -73,4 +72,27 @@ class Bot:
         pass
 
     def start(self):
-        pass
+        token = self.token
+        if "description" in dir(self) and "prefix" in dir(self):
+            bot = commands.Bot(command_prefix=self.prefix, description=self.description)
+        elif "prefix" in dir(self):
+            bot = commands.Bot(command_prefix=self.prefix, description="")
+        else:
+            bot = commands.Bot(command_prefix="", description="")
+
+        @bot.event
+        async def on_ready():
+            if "onready" in dir(self):
+                eval(self.onready)
+
+        # User running custom code (self.cc1-1000)
+        cc = []
+        for i in range(1000):
+            if "cc" + str(i) in dir(self):
+                cc.append("cc" + str(i))
+        for customcode in cc:
+            code = "self." + customcode
+            exec(eval(code))
+
+
+        bot.run(token)
